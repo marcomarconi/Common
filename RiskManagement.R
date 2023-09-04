767c51d302f8c19312eaffda3c12aadf5e007272
marco.marconi <marco.marconi@gmail.com>
2023-09-04 16:23:06 +0200

    o

--- RiskManagement.R
+++ RiskManagement.R
@@ -27,7 +27,7 @@ cap_forecast <- function(x, cap=20) {
 
 decimalplaces <- function(x) {
     if ((x %% 1) != 0) {
-        nchar(strsplit(sub('0+$', '', as.character(x)), ".", fixed=TRUE)[[1]][[2]])
+        nchar(strsplit(sub('0+$', '', format(x, scientific = FALSE)), ".", fixed=TRUE)[[1]][[2]])
     } else {
         return(0)
     }
